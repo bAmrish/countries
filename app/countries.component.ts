9964ec2b4724947165d9727a68a5dc2ca4978eab
Amrish Bharatiya <bamrish@gmail.com>
2016-03-04 17:40:21 -0500

    Initial shell structure for the application

---
+++ app/countries.component.ts
@@ -0,0 +1,11 @@
+import {Component} from 'angular2/core';
+
+
+@Component ({
+    selector: 'my-app',
+    template: '<h1> This is Countries </h1>'
+})
+
+export class Countries {
+    
+}
---
+++ app/main.ts
@@ -0,0 +1,4 @@
+import {bootstrap} from 'angular2/platform/browser';
+import {Countries} from './countries.component';
+
+bootstrap(Countries);
