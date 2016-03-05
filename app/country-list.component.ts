28528489c238c65bcd73d868528d2e2abd827997
Amrish Bharatiya <bamrish@gmail.com>
2016-03-05 11:26:27 -0500

    Added link to route from country list view to details view

--- app/country-list.component.ts
+++ app/country-list.component.ts
@@ -2,11 +2,13 @@ import {Component, OnInit} from 'angular2/core';
 import {Country} from './country';
 import {CountryService} from './country.service'
 import {MapLinkPipe} from './map-link.pipe';
+import {ROUTER_DIRECTIVES} from 'angular2/router';
 
 @Component ({
     selector: 'country-list',
     templateUrl: 'app/country-list.component.html',
-    pipes: [MapLinkPipe]
+    pipes: [MapLinkPipe],
+    directives: [ROUTER_DIRECTIVES]
 })
 export class CountryListComponent implements OnInit {
 
@@ -24,5 +26,4 @@ export class CountryListComponent implements OnInit {
     onSearch() {
         this.countries = this._countryService.searchCountries(this.searchTerm);
     }
-
 }
