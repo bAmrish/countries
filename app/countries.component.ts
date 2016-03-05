d401564f4c68e83dc1c638762685d29ae0a91f44
Amrish Bharatiya <bamrish@gmail.com>
2016-03-04 19:25:26 -0500

    Added a pipe for creating map link given lattitued and longitude

--- app/countries.component.ts
+++ app/countries.component.ts
@@ -1,11 +1,13 @@
 import {Component, OnInit} from 'angular2/core';
 import {Country} from './country';
 import {CountryService} from './country.service'
+import {MapLinkPipe} from './map-link.pipe';
 
 @Component ({
     selector: 'my-app',
     templateUrl: 'app/countries.component.html',
-    providers: [CountryService]
+    providers: [CountryService],
+    pipes: [MapLinkPipe]
 })
 export class Countries {
     constructor(
---
+++ app/map-link.pipe.ts
@@ -0,0 +1,12 @@
+import {Pipe, PipeTransform} from 'angular2/core';
+
+
+@Pipe({name: 'mapLink'}) 
+export class MapLinkPipe implements PipeTransform {
+    transform(value: number[], args: string[]): string{
+        let lat = value[0];
+        let long = value[1];
+     //   return "https://www.google.com/maps/preview/@" + lat + "," + long + ",5z";
+        return 'https://www.google.com/maps/place/' +lat +',' + long + '/@' + lat + ',' + long + ',5z';
+    }
+}
